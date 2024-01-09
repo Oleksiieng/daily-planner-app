dc424efe801f6a8477d98beca5413acaa2ccf8db
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-01-09 20:37:41 +0000

    added table layout

--- js/logic.js
+++ js/logic.js
@@ -2,4 +2,19 @@ $(document).ready(function () {
 
     let currentDate = dayjs().format('dddd, MMMM D, YYYY');
     $('#currentDay').text(currentDate);
+
+
+
+    var currentHour = dayjs().hour();
+    $('.time-block').each(function() {
+        var blockHour = parseInt($(this).find('.hour').text());
+        if(blockHour < currentHour) {
+            $(this).find('.description').addClass('past');
+        } else if(blockHour === currentHour) {
+            $(this).find('.description').addClass('present');
+        } else {
+            $(this).find('.description').addClass('future');
+        }
+    });
+
 });
