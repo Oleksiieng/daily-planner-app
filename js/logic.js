bd36605b261275456c708bef7895b21815d8edb5
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-01-09 21:14:41 +0000

    refactored table layout

--- js/logic.js
+++ js/logic.js
@@ -3,18 +3,29 @@ $(document).ready(function () {
     let currentDate = dayjs().format('dddd, MMMM D, YYYY');
     $('#currentDay').text(currentDate);
 
+    function generateTimeBlocks() {
+        let times = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+        let container = $('.container');
 
+        times.forEach(function (time) {
+           
+            let timeBlock = $('<div>').addClass('row time-block');
 
-    var currentHour = dayjs().hour();
-    $('.time-block').each(function() {
-        var blockHour = parseInt($(this).find('.hour').text());
-        if(blockHour < currentHour) {
-            $(this).find('.description').addClass('past');
-        } else if(blockHour === currentHour) {
-            $(this).find('.description').addClass('present');
-        } else {
-            $(this).find('.description').addClass('future');
-        }
-    });
+            let hourColumn = $('<div>').addClass('col-md-1 hour').text(time);
 
+            let textArea = $('<textarea>').addClass('col-md-10 description').attr('data-hour', time);
+
+            let saveButton = $('<button>').addClass('btn saveBtn col-md-1').html('<i class="fas fa-save"></i>');
+            saveButton.on('click', function () {
+                let hour = $(this).siblings('.description').data('hour');
+                let text = $(this).siblings('.description').val();
+            });
+
+            timeBlock.append(hourColumn, textArea, saveButton);
+            container.append(timeBlock);
+           
+        });
+    }
+
+    generateTimeBlocks();
 });
