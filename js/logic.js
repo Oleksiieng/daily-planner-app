32e4b398d7f8a2b6abd6a049b0a8ffb684ab44ce
Oleksii Kulykov <oleksii.eng@gmail.com>
2024-01-09 21:22:22 +0000

    added save events for buttons

--- js/logic.js
+++ js/logic.js
@@ -3,12 +3,19 @@ $(document).ready(function () {
     let currentDate = dayjs().format('dddd, MMMM D, YYYY');
     $('#currentDay').text(currentDate);
 
+
+    function saveEvent(hour, text) {
+        let date = dayjs().format('YYYY-MM-DD');
+        let key = date + '-' + hour;
+        localStorage.setItem(key, text);
+    }
+
     function generateTimeBlocks() {
         let times = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
         let container = $('.container');
 
         times.forEach(function (time) {
-           
+
             let timeBlock = $('<div>').addClass('row time-block');
 
             let hourColumn = $('<div>').addClass('col-md-1 hour').text(time);
@@ -19,11 +26,12 @@ $(document).ready(function () {
             saveButton.on('click', function () {
                 let hour = $(this).siblings('.description').data('hour');
                 let text = $(this).siblings('.description').val();
+                saveEvent(hour, text);
             });
 
             timeBlock.append(hourColumn, textArea, saveButton);
             container.append(timeBlock);
-           
+
         });
     }
 
